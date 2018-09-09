343c726253e0b8167105101346492a52a64a2ba7
arascan35 <arascan.35@outlook.com>
2018-09-09 16:38:36 +0300

    Refactor Service Interface to Common Base Interface.closes #11

--- pet-clinic-data/src/main/java/canaras/springframework/springPetClinic/services/PetService.java
+++ pet-clinic-data/src/main/java/canaras/springframework/springPetClinic/services/PetService.java
@@ -2,9 +2,6 @@ package canaras.springframework.springPetClinic.services;
 
 import canaras.springframework.springPetClinic.Model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet,Long> {
-
 
 }
