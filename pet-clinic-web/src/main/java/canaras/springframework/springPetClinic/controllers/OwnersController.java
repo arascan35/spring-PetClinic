8887fc4d8eb9710528f36fd7b5af11587f769c7d
arascan35 <arascan.35@outlook.com>
2018-09-09 19:40:12 +0300

    Owners Index page created.closes #13

---
+++ pet-clinic-web/src/main/java/canaras/springframework/springPetClinic/controllers/OwnersController.java
@@ -0,0 +1,13 @@
+package canaras.springframework.springPetClinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"/owners","/owners/index.html","/owners/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
